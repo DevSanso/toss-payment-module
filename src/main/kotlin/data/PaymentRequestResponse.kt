b9c2ce695db14756d71ed69b867a28c7d72284da
Sunken Ahn <devsanso@outlook.com>
2022-02-23 21:59:36 +0900

    토스 http body 포멧

--- src/main/kotlin/data/PaymentRequestResponse.kt
+++ src/main/kotlin/data/PaymentRequestResponse.kt
@@ -4,20 +4,9 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class PaymentRequestResponse(
-    val status : String, //결제 완료
-    val payToken : String, //승인된 결제 토큰
-    val orderNo : String, //결제생성 구간에서 전달된 가맹점 주문번호
-    val payMethod : String, //승인된 결제수단
-    val amount : Int, //결제요청된 금액
-    val discountedAmount : Int, //할인된 금액
-    val paidAmount : Int, //지불수단 승인금액
-    val paidTs : String, //결제 완료 처리 시간
-    val transactionId : String, //거래 트랜잭션 아이디
-    val cardCompanyCode : String, //승인된 카드사 코드
-    val cardAuthorizationNo : String, //카드 승인번호
-    val spreadOut : String, //사용자가 선택한 카드 할부개월
-    val noInterest: Boolean, //카드 무이자 적용 여부
-    val cardMethodType : String, //카드 타입
-    val cardNumber : String, //마스킹된 카드번호
-    val salesCheckLinkUrl : String //신용카드 매출전표 호출URL
+    val code : Int, //응답코드
+    val checkoutPage : String, //결제를 진행할 수 있는 토스페이 웹페이지 URL 토스가 전달한 값 그대로를 구매자에게 띄워주세요.
+    val payToken : String, //토스페이 토큰 가맹점에서는 이 값을 반드시 저장하고 관리하셔야 합니다.
+    val msg : String, //응답이 성공이 아닌 경우 설명 메세지
+    val errorCode : String //에러 코드
 )
